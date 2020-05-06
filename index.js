7d90b78ee012997ae56847a4093e49af30e6cda5
GoffetteDorian <goffettedorian@gmail.com>
2020-05-06 10:57:01 +0200

    adding axios dependencies

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 
-console.log("Hello, Node.JS!");
-console.log(process.argv.slice(2));
+const { getCode, getName } = require("country-list");
+
+const countryName = process.argv.slice(2)[0];
+if (countryName) {
+  const countryCode = getCode(countryName);
+  if (countryCode) {
+    console.log(countryCode);
+  } else {
+    console.error("There may be an error with your country name");
+  }
+} else {
+  console.error("Whoops! You did not enter a country name");
+}
